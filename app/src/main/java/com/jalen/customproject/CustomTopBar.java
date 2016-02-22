f97a543c01580f4838820517e0cb3b29f1e81366
于德海 <yudehai0204@gmail.com>
2016-02-22 19:00:51 +0800

    修复不显示问题

--- app/src/main/java/com/jalen/customproject/CustomTopBar.java
+++ app/src/main/java/com/jalen/customproject/CustomTopBar.java
@@ -55,6 +55,7 @@ public class CustomTopBar extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        measureChildren(widthMeasureSpec,heightMeasureSpec);
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSize = MeasureSpec.getSize(widthMeasureSpec);
 
@@ -65,19 +66,27 @@ public class CustomTopBar extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         int count = getChildCount();
+        int cr,cb,cl,ct;
+        cb = b;
+        cl=0;
+        ct=0;
+        cr=r;
         for(int i=0;i<count;i++){
             View child = getChildAt(i);
+            int width = child.getMeasuredWidth();
             switch (i){
-                case 0:
-                    child.layout(0,0,200,200);
+                case 0://左
+                    cl=ct=0;
                     break;
-                case 1:
-                    child.layout(1000,0,1440,200);
+                case 1://右
+                    cl = r-width;
                     break;
-                case 2:
-                    child.layout(500,0,900,200);
+                case 2://中
+
+                    cl= r/2-width/2;
                     break;
             }
+            child.layout(cl,ct,cl+width,b);
         }
     }
 
